dfd828cd4044bb19ef337f8a50ef9264013e6356
Qiqi Wang <qiqi@mit.edu>
2014-05-13 16:38:43 -0400

    fixed adsparse for python not just ipython

--- adsparse.py
+++ adsparse.py
@@ -2,6 +2,7 @@ import os
 import sys
 import unittest
 import numbers
+import pylab
 import numpy as np
 import scipy.sparse as sp
 import scipy.sparse.linalg as splinalg
@@ -13,7 +14,7 @@ from numpad.adarray import *
 from numpad.adarray import __DEBUG_MODE__, _DEBUG_perturb_new
 from numpad.adsolve import ResidualState, SolutionState, adsolution, solve
 
-class sparse_matrix:
+class csr_matrix:
     '''
     Sparse matrix that can be automatically differentiated.
     e.g.,
@@ -36,6 +37,46 @@ class sparse_matrix:
             self.shape = (i.max() + 1, j.max() + 1)
 
         self._base = sp.csr_matrix((self.data._base, (i, j)), shape=self.shape)
+
+    def __mul__(self, b):
+        '''
+        Only implemented for a single vector b
+        '''
+        assert b.ndim == 1
+        A_x_b = adarray(self._base * b._base)
+
+        A_x_b.next_state(self._base, b, '*')
+
+        data_multiplier = sp.csr_matrix((b._base[self.j],
+                                         (self.i, np.arange(self.data.size))))
+        A_x_b.next_state(data_multiplier, self.data, '*')
+
+        return A_x_b
+
+
+class bsr_matrix:
+    '''
+    Sparse matrix that can be automatically differentiated.
+    e.g.,
+    A = bsr_matrix(data, i, j, shape)
+    u = spsolve(A, b)
+    J = dot(c, u)
+    J.diff(data)
+
+    The constructor
+    A = sparse_matrix(data, i, j, shape)
+    is similar to the following in scipy.sparse
+    A = csr_matrix((data, (i, j)), shape)
+    '''
+    def __init__(self, data, i, j, shape=None):
+        self.data = data.copy()
+        self.i = i.copy()
+        self.j = j.copy()
+        self.shape = shape
+        if shape is None:
+            self.shape = (i.max() + 1, j.max() + 1)
+
+        self._base = sp.bsr_matrix((self.data._base, (i, j)), shape=self.shape)
 
     def __mul__(self, b):
         '''
@@ -83,7 +124,7 @@ if __name__ == '__main__':
     u = solve(resid, ones(N-1))
     J = u.sum()
     adj = np.array(J.diff(a)).ravel()
-    plot(adj)
+    pylab.plot(adj)
 
     # sparse matrix way
     def tridiag(a):
@@ -96,7 +137,7 @@ if __name__ == '__main__':
         diag = -(a[:-1] + a[1:]) / dx**2
         i_diag, j_diag = np.arange(N-1), np.arange(N-1)
 
-        A = sparse_matrix(hstack([lower, upper, diag]),
+        A = csr_matrix(hstack([lower, upper, diag]),
                           np.hstack([i_lower, i_upper, i_diag]),
                           np.hstack([j_lower, j_upper, j_diag]))
         return A
@@ -104,7 +145,7 @@ if __name__ == '__main__':
     u1 = spsolve(tridiag(a), b)
     J1 = u1.sum()
     adj1 = np.array(J1.diff(a)).ravel()
-    plot(adj1)
+    pylab.plot(adj1)
 
     # finite difference
     fd = np.zeros(a.size)
@@ -115,8 +156,8 @@ if __name__ == '__main__':
         du = sp.linalg.spsolve(A._base, b._base) - u._base
         fd[i] = du.sum() / 1E-6
 
-    plot(fd)
+    pylab.plot(fd)
 
-    print('Adj - Adj1', norm(adj - adj1))
-    print('Adj - fd', norm(adj - fd))
-    print('Adj1 - fd', norm(adj1 - fd))
+    print('Adj - Adj1', np.linalg.norm(adj - adj1))
+    print('Adj - fd', np.linalg.norm(adj - fd))
+    print('Adj1 - fd', np.linalg.norm(adj1 - fd))
